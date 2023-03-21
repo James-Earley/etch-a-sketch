c1429d6f2508e6bbef5765f33c982f8ffcfa88de
James Earley <james.albert.earley@gmail.com>
2023-03-21 18:16:47 +0000

    Major functionality working with grid and hover over effects, button added but not linked to JS

--- scripts.js
+++ scripts.js
@@ -1,10 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-const flexContainer = document.getElementById("flex-box");
-  for (let i = 1; i <= 16; i++) {
-    const div = document.createElement("div");
-    div.id = "div" + i;
-    div.className = "flex-div";
-    div.textContent = "div" + i;
-    flexContainer.appendChild(div);
+document.addEventListener("DOMContentLoaded", function() {
+  const gridNumber = 16;
+  const container = document.getElementById("container");
+  container.style.display = "flex";
+
+  for (let i = 1; i <= gridNumber; i++) {  
+    const column = document.createElement("div");
+    container.appendChild(column).className = "gridColumn";
+
+    for (let j = 1; j <= gridNumber; j++) {
+      const row = document.createElement("div");
+      row.style.display = "flex";
+      row.style.minHeight = "30px";
+      row.style.maxHeight = "40px";
+      column.appendChild(row).className = "cell";
+    }
+     
   }
+  
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const gridCells = document.querySelectorAll("div.cell");
+  console.log(gridCells);
+  gridCells.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.classList.add("hover");
+    });
+
+    //item.addEventListener("mouseout", () => {
+    //  item.classList.remove("hover");
+    //});
+  });
 });
+
+
